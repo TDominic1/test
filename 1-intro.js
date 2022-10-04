c6e357053a31d8a661e2fda1eb591fde8042ebee
TDominic1 <103081029+TDominic1@users.noreply.github.com>
2022-10-04 21:40:30 +1300

    First commit

---
+++ 1-intro.js
@@ -0,0 +1,11 @@
+const amount = 9;
+
+if(amount<10){
+    console.log("smaler number");
+
+}
+else{
+    console.log("bigger number");
+}
+
+console.log("Hello world")
---
+++ 3-modules.js
@@ -0,0 +1,13 @@
+//modules - Encapsulated code (only share minimum )
+// CommonJs, every file is module (by default)
+
+const names = require("./4-names")
+const sayHi = require("./5-utils")
+const data = require("./6-alternative-flavour")
+
+
+require("./7-mind-grenade")
+
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
